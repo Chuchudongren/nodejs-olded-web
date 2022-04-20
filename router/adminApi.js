450273445cd2615c2fe86f8d8d0a577e2403644b
guojie <guojie@86.com>
2022-04-20 18:10:54 +0800

    增加了后台用户管理的Api

--- router/adminApi.js
+++ router/adminApi.js
@@ -5,8 +5,11 @@ const router = express.Router()
 const AdminHandle = require('../router_handle/adminApi')
 
 router.post('/login', AdminHandle.login)
-router.post('/user', AdminHandle.user)
 router.post('/getRoleRights', AdminHandle.getRoleRights)
+router.get('/user', AdminHandle.user)
+router.post('/changeRoleById', AdminHandle.changeRoleById)
+router.post('/disabledUser', AdminHandle.disabledUser)
+router.post('/addAdmin', AdminHandle.addAdmin)
 
 // 将路由器 共享出去
 module.exports = router
--- router_handle/adminApi.js
+++ router_handle/adminApi.js
@@ -16,15 +16,7 @@ exports.login = (req, res) => {
         token: jwt.sign({ username: username }, jwtSecretKey, { expiresIn })
     })
 }
-exports.user = (req, res) => {
-    const username = req.body.username
-    const password = req.body.password
-    res.send({
-        status: 200,
-        message: '登录成功！',
-        result: '123'
-    })
-}
+
 function roleRight(results) {
     let newResults = results.filter(item => { return item.grade === 1 })
     newResults.map(res => {
@@ -51,5 +43,63 @@ exports.getRoleRights = (req, res) => {
         } else {
             res.cc('未找到数据！', 401)
         }
+    })
+}
+
+exports.user = (req, res) => {
+    const selectSql = `select a.adminid,a.username,a.password,a.roleid,b.rolename,a.state 
+    from admin a
+    left join roles b
+    on a.roleid = b.roleid`
+    db.query(selectSql, (err, results) => {
+        if (err) return res.cc(err)
+        if (results.length > 0) {
+            res.send({
+                status: 200,
+                results
+            })
+        }
+    })
+}
+exports.changeRoleById = (req, res) => {
+    const adminid = req.body.adminid
+    const rolename = req.body.rolename
+    const password = req.body.password
+    const username = req.body.username
+    const updateSql = `update admin set roleid = (select roleid from roles where rolename = ?),username = ?,password = ? where adminid = ?`
+    db.query(updateSql, [rolename, username, password, adminid], (err, result) => {
+        if (err) return res.cc(err)
+        if (result.affectedRows === 1) {
+            res.cc('修改成功！', 200)
+        } else {
+            res.cc('修改失败！', 200)
+        }
+    })
+}
+exports.disabledUser = (req, res) => {
+    const adminid = req.body.adminid
+    const state = req.body.state === 0 ? 1 : 0
+    const updateSql = `update admin set state = ? where adminid = ?`
+    db.query(updateSql, [state, adminid])
+}
+exports.addAdmin = (req, res) => {
+    const username = req.body.username
+    const password = req.body.password
+    const rolename = req.body.rolename
+    const insertSql = `insert into admin (username,password,roleid) values (?,?,(select roleid from roles where rolename = ?))`
+    const selectSql = `select * from admin where adminid = ?`
+    db.query(insertSql, [username, password, rolename], (err, result) => {
+        if (err) return res.cc(err)
+        if (result.affectedRows === 1) {
+            db.query(selectSql, [result.insertId], (err1, result1) => {
+                if (err1) return res.cc(err1)
+                res.send({
+                    status: 200,
+                    message: '添加成功！',
+                    newData: result1[0]
+                })
+            })
+
+        } else res.cc('添加失败', 200)
     })
 }
